a06b75661d1cd49cae762d6c9994b0c6b7fa092c
Ma Wing Kin <mwk0048@gmail.com>
2023-03-21 23:45:03 +0800

    test lambda function

--- backend/set_visit_count.py
+++ backend/set_visit_count.py
@@ -6,6 +6,21 @@ lambda_client = boto3.client('lambda')
 
 
 def lambda_handler(event, context):
+    id = event.get('queryStringParameters', {}).get('id', '0')
+    if not (id == '0' or id == '1'):
+        return {
+            'statusCode': 400,
+            'isBase64Encoded': False,
+            'headers': {
+                'Content-Type': 'application/json',
+                'Access-Control-Allow-Origin': '*',  # CORS
+            },
+            'body': json.dumps(
+                {
+                    'message': 'Invalid id',
+                }
+            )
+        }
     data = lambda_client.invoke(FunctionName='get_visit_count',
                                 InvocationType='RequestResponse', Payload=json.dumps(event))
     # print(data.keys())
@@ -13,7 +28,6 @@ def lambda_handler(event, context):
     json_data = json.loads(data['Payload'].read())
     body = json.loads(json_data['body'])
     count = body['count']
-    id = event.get('queryStringParameters', {}).get('id', '0')
     client.put_item(TableName='visit_count', Item={
                     'id': {'N': str(id)}, 'count': {'N': str(count+1)}})
     return {
